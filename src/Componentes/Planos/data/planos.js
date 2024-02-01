46ddb4ae7711c0240ecc6df1d5d177f20e0fc076
Matheus Link <linksauermths2203@gmail.com>
2024-02-01 18:48:35 -0300

    Adicionando e renderizando planos

---
+++ src/Componentes/Planos/data/planos.js
@@ -0,0 +1,22 @@
+export const planos = [
+  {
+    nome: "EcoBasic",
+    beneficios: [
+      "Até 15% de desconto em nossa loja",
+      "Um presente a cada 12 meses de assinatura",
+      "EcoFlix",
+    ],
+    precoMensal: "4.99",
+    precoAnual: "49.99",
+  },
+  {
+    nome: "EcoPlus",
+    beneficios: [
+      "Até 30% de desconto em nossa loja",
+      "Dois presentes a cada 12 meses de assinatura",
+      "EcoFlix+",
+    ],
+    precoMensal: "8.99",
+    precoAnual: "89.99",
+  },
+];
--- src/Componentes/Planos/index.jsx
+++ src/Componentes/Planos/index.jsx
@@ -1,7 +1,31 @@
+import { planos } from "./data/planos"
+
 const Planos = () => {
   return (
     <main>
       <h1>Planos</h1>
+      <article>
+        {planos.map((plano, index)=>{
+          return(
+            <section key={index}>
+              <h2>{plano.nome}</h2>
+              <ul>
+                <li>{plano.beneficios[0]}</li>
+                <li>{plano.beneficios[1]}</li>
+                <li>{plano.beneficios[2]}</li>
+              </ul>
+              <div>
+                <h4>{plano.precoMensal}</h4>
+                <span>Mensal</span>
+              </div>
+              <div>
+                <h4>{plano.precoAnual}</h4>
+                <span>Anual</span>
+              </div>
+            </section>
+          )
+        })}
+      </article>
     </main>
   )
 }
