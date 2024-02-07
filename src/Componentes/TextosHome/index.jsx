aa591e8866395bc891c92fef5219591cb4662e6c
Matheus Link <linksauermths2203@gmail.com>
2024-02-07 13:38:43 -0300

    estilos: corrigindo estilos dos produtos e estilizando componentes da home

--- src/Componentes/TextosHome/index.jsx
+++ src/Componentes/TextosHome/index.jsx
@@ -1,10 +1,11 @@
 import { texto1, texto2, texto3 } from "./textosHome";
+import './style.scss'
 
 const TextosHome = () => {
   return (
     <>
     <h1>Home</h1>
-    <article>
+    <article className="lista-textos-home">
       <section>
         <p>{texto1}</p>
       </section>
